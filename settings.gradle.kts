dcbd2be75bbc706870ccc6d0633529d488fdb7b1
Logesh Kumar R S <logeshkumar2986@gmail.com>
2025-11-09 06:49:38 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,8 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven(url = "https://maven.kr328.app/releases")
         maven (url = "https://jitpack.io")
+        maven(url = "https://maven.kr328.app/releases")
     }
 }
 dependencyResolutionManagement {
